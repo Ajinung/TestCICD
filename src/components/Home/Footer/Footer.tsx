c5119860ed05444aee61357d70f493a8a19281bd
Ajinung <isaacetor7@gmail.com>
2023-03-08 16:16:33 +0100

    added asset folder

--- src/components/Home/Footer/Footer.tsx
+++ src/components/Home/Footer/Footer.tsx
@@ -4,15 +4,50 @@ import styled from "styled-components";
 const Footer = () => {
   return (
     <div>
-      <Container></Container>
+      <Container>
+        <Wrapper>
+          <Resource></Resource>
+          <Community></Community>
+          <About></About>
+        </Wrapper>
+      </Container>
     </div>
   );
 };
 
 export default Footer;
 
+const About = styled.div`
+  width: 30%;
+  height: 100%;
+  background-color: #fff;
+`;
+
+const Community = styled.div`
+  width: 30%;
+  height: 100%;
+  background-color: #fff;
+`;
+
+const Resource = styled.div`
+  width: 30%;
+  height: 100%;
+  background-color: #fff;
+`;
+
+const Wrapper = styled.div`
+  width: 95%;
+  height: 95%;
+  background-color: red;
+  display: flex;
+  justify-content: space-between;
+`;
+
 const Container = styled.div`
   width: 100%;
-  height: 15vh;
+  height: 35vh;
   background-color: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
