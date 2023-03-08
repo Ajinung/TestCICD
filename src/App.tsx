2f5be68a56ae5480681d56c4cbdb691cbe9988c7
Ajinung <isaacetor7@gmail.com>
2023-03-08 15:34:12 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Home/Header/Header";
+import Footer from "./components/Home/Footer/Footer";
+import Hero from "./components/Home/Hero/Hero";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Hero />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header/Header";
+import Hero from "./Hero/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
