ea4a1ea373a7f01acd98fba06b1c563ffcfbbbad
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-08 16:16:17 +0100

    saved styling

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -1,16 +1,33 @@
 import React from "react";
+import pic from "../git.png";
+import styled from "styled-components";
 
 const Hero = () => {
   return (
     <div>
-      <div
-        style={{ backgroundColor: "aliceblue", height: "500px", width: "100%" }}
-      >
-        this is container
-        <div>first text</div>
-      </div>
+      <Container>
+        <Wrapper></Wrapper>
+      </Container>
     </div>
   );
 };
 
 export default Hero;
+
+const Wrapper = styled.div``;
+
+const Container = styled.div`
+  width: 100%;
+  height: 500px;
+  background-color: lightgrey;
+  background-image: url(${pic});
+  background-repeat: no-repeat;
+  background-position: center;
+position: relative;
+  ::before{
+    width:100%;
+    height:100%;
+    background-color : rgba(0,0,0,0.6)
+    position: absolute;
+  }
+`;
