2e587b7199552d0c3499d2d8981d7ad28cd5af64
Favour-Yusuf <favouryusuf45@gmail.com>
2023-03-08 17:23:09 +0100

    header done

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import pic from "../../Assets/logo2.png";
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <Container>
       <Wrapper>
-        <Logo />
+        <Logo src={pic} />
         <Navbar>
-          <Products>Products</Products>
-          <Contacts>Contacts</Contacts>
-          <About>About</About>
+          <Nav>Products</Nav>
+          <Nav>Contacts</Nav>
+          <Nav>About</Nav>
         </Navbar>
         <Last>
           <Button>SignIn</Button>
@@ -25,26 +26,45 @@ export default Header;
 const Last = styled.div`
   display: flex;
   justify-content: space-between;
+  width: 320px;
 `;
 const Button = styled.button`
   height: 40px;
   width: 150px;
   background-color: #6666ce;
   border: none;
+  border-radius: 10px;
+  color: white;
+  font-size: 17px;
+  cursor: pointer;
+
+  :hover {
+    background-color: #9090fa;
+    color: black;
+    transition: all 350ms;
+  }
 `;
-const Contacts = styled.div``;
-const About = styled.div``;
-const Products = styled.div``;
+const Nav = styled.div`
+  cursor: pointer;
+`;
 
 const Wrapper = styled.div`
   width: 80%;
   height: 100%;
-  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+  /* box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px; */
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
-const Logo = styled.div``;
-const Navbar = styled.div``;
+const Logo = styled.img`
+  height: 80px;
+`;
+const Navbar = styled.div`
+  display: flex;
+  font-size: 20px;
+  justify-content: space-between;
+  width: 400px;
+`;
 const SignIn = styled.div``;
 const Container = styled.div`
   height: 70px;
