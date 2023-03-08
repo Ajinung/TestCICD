7387dadf61d042463736b4e611b83f7772c7c94c
Ajinung <isaacetor7@gmail.com>
2023-03-08 16:31:52 +0100

    footer contents

--- src/components/Home/Footer/Footer.tsx
+++ src/components/Home/Footer/Footer.tsx
@@ -6,9 +6,30 @@ const Footer = () => {
     <div>
       <Container>
         <Wrapper>
-          <Resource></Resource>
-          <Community></Community>
-          <About></About>
+          <Resource>
+            <h1>RESOURCES</h1>
+            <p>Docs</p>
+            <p>Learn</p>
+            <p>Guides</p>
+            <p>API Reference</p>
+            <p>Blog</p>
+          </Resource>
+          <Community>
+            <h1>COMMUNITY</h1>
+            <p>User Showcase</p>
+            <p>Funding</p>
+            <p>Guides</p>
+            <p>API Reference</p>
+            <p>Blog</p>
+          </Community>
+          <About>
+            <h1>ABOUT</h1>
+
+            <p>Home</p>
+            <p>GitHub</p>
+            <p>Twitter</p>
+            <p>Contact Sales</p>
+          </About>
         </Wrapper>
       </Container>
     </div>
@@ -20,25 +41,24 @@ export default Footer;
 const About = styled.div`
   width: 30%;
   height: 100%;
-  background-color: #fff;
+  /* background-color: #fff; */
 `;
 
 const Community = styled.div`
   width: 30%;
   height: 100%;
-  background-color: #fff;
+  /* background-color: #fff; */
 `;
 
 const Resource = styled.div`
   width: 30%;
   height: 100%;
-  background-color: #fff;
+  /* background-color: #fff; */
 `;
 
 const Wrapper = styled.div`
   width: 95%;
   height: 95%;
-  background-color: red;
   display: flex;
   justify-content: space-between;
 `;
@@ -50,4 +70,5 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  color: #fff;
 `;
